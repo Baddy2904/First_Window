3472707cb5b64febf2dbb68c472276832e425e2c
Baddy2904 <99268410+Baddy2904@users.noreply.github.com>
2022-08-16 16:34:50 +0200

    Add files via upload

---
+++ secondWindow.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import tkinter as tk
+
+class secondWindow():
+
+    def __init__(self, window2):
+        """
+        """
+        self.window2 = window2
+
+    def ciao(self):
+        """
+        """
+        print("ciao2")
